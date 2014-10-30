3c8a31d84eb5f02534b98e4a19ca5753e7f064a8
jxv <joevargas@hush.com>
2014-10-30 14:55:57 -0700

    Zb0t.HoldEm just needed to derive Ord instance >_<

--- src/Zb0t/HoldEm.hs
+++ src/Zb0t/HoldEm.hs
@@ -34,16 +34,16 @@ data Card = Card { rank :: Rank, suit :: Suit }
 
 
 data Hand
-    = SFlush    Rank
-    | Kind4     Rank Rank
-    | FHouse    Rank Rank
-    | Flush     Rank Rank Rank Rank Rank
-    | Straight  Rank
-    | Kind3     Rank Rank Rank
-    | Pair2     Rank Rank Rank
+    = High      Rank Rank Rank Rank Rank
     | Pair1     Rank Rank Rank Rank
-    | High      Rank Rank Rank Rank Rank
-    deriving (Show, Eq)
+    | Pair2     Rank Rank Rank
+    | Kind3     Rank Rank Rank
+    | Straight  Rank
+    | Flush     Rank Rank Rank Rank Rank
+    | FHouse    Rank Rank
+    | Kind4     Rank Rank
+    | SFlush    Rank
+    deriving (Show, Eq, Ord)
 
 
 type PHand = (Card, Card)
@@ -72,69 +72,6 @@ instance Show Card where
 
 instance Ord Card where
     compare (Card a _) (Card v _) = compare a v
-
-
-instance Ord Hand where
-    compare (SFlush a) = \case
-        SFlush b -> compare a b
-        _ -> GT
-    compare (Kind4 a b) = \case
-        Kind4 v w -> compare2 (a,b) (v,w)
-        SFlush{} -> LT
-        _ -> GT
-    compare (FHouse a b) = \case
-        FHouse v w -> compare2 (a,b) (v,w)
-        SFlush{} -> LT
-        Kind4{} -> LT
-        _ -> GT
-    compare (Flush a b c d e) = \case
-        Flush v w x y z -> compare5 (a,b,c,d,e) (v,w,x,y,z)
-        SFlush{} -> LT
-        Kind4{} -> LT
-        FHouse{} -> LT
-        _ -> GT
-    compare (Straight a) = \case
-        Straight v -> compare a v
-        SFlush{} -> LT
-        Kind4{} -> LT
-        FHouse{} -> LT
-        Flush{} -> LT
-        _ -> GT
-    compare (Kind3 a b c) = \case
-        Kind3 v w x -> compare3 (a,b,c) (v,w,x)
-        Pair2{} -> GT
-        Pair1{} -> GT
-        High{} -> GT
-        _ -> LT
-    compare (Pair2 a b c) = \case
-        Pair2 v w x -> compare3 (a,b,c) (v,w,x)
-        Pair1{} -> GT
-        High{} -> GT
-        _ -> LT
-    compare (Pair1 a b c d) = \case
-        Pair1 v w x y -> compare4 (a,b,c,d) (v,w,x,y)
-        High{} -> GT
-        _ -> LT
-    compare (High a b c d e) = \case
-        High v w x y z -> compare5 (a,b,c,d,e) (v,w,x,y,z)
-        _ -> LT
-
-
-compare2 :: (Rank, Rank) -> (Rank, Rank) -> Ordering
-compare2 (a,b) (v,w) = case compare a v of EQ -> compare b w; o -> o
-
-
-compare3 :: (Rank, Rank, Rank) -> (Rank, Rank, Rank) -> Ordering
-compare3 (a,b,c) (v,w,x) = case compare a v of EQ -> compare2 (b,c) (w,x); o -> o
-
-
-compare4 :: (Rank, Rank, Rank, Rank) -> (Rank, Rank, Rank, Rank) -> Ordering
-compare4 (a,b,c,d) (v,w,x,y) = case compare a v of EQ -> compare3 (b,c,d) (w,x,y); o -> o
-
-
-compare5 :: (Rank, Rank, Rank, Rank, Rank) -> (Rank, Rank, Rank, Rank, Rank) -> Ordering
-compare5 (a,b,c,d,e) (v,w,x,y,z) =
-    case compare a v of EQ -> compare4 (b,c,d,e) (w,x,y,z); o -> o
 
 
 deck :: [Card]
