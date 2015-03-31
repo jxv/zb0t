da826852d8038a2bd66c65f5c3c4efdba478e8b4
jxv <joevargas@hush.com>
2015-03-31 10:19:08 -0700

    Init switch from ByteString to Text

--- src/Zb0t/Core.hs
+++ src/Zb0t/Core.hs
@@ -17,6 +17,9 @@ import qualified Data.ByteString.Char8 as BSC
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.Lazy.Char8 as BLC
 import qualified Data.List as L
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import qualified Data.Text.Lazy as TL
 import qualified System.Random as Random
 import qualified Text.Parsec as Parsec
 import qualified Text.Parsec.Combinator as Parsec
@@ -31,6 +34,7 @@ import Control.Concurrent.Async (async, wait)
 import Control.Monad (when, void)
 import Control.Monad.Trans (liftIO)
 import Data.ByteString (ByteString)
+import Data.Text (Text)
 import GHC.IO.Handle (Handle)
 import System.IO (hPutStrLn,hGetLine)
 import System.IO.Error (catchIOError)
@@ -42,7 +46,7 @@ data CommandTag
     | PRIVMSG
     deriving (Show, Read, Eq, Enum, Bounded)
 
-type Parser = Parsec.ParsecT ByteString () IO
+type Parser = Parsec.ParsecT Text () IO
 
 connect :: Config -> IO (Maybe Handle)
 connect (Config srv port _ _ _) = do
@@ -56,38 +60,38 @@ run cfg@(Config _ _ chans nck mpswd) = Net.withSocketsDo $ do
         Nothing -> putStrLn "No connection."
         Just handle -> do
             chan <- newChan
-            login nck mpswd chan
-            joinChannel chans chan
+            login (T.decodeUtf8 nck) (fmap T.decodeUtf8 mpswd) chan
+            joinChannel (map T.decodeUtf8 chans) chan
             thdrecv <- forkIO (recv handle chan)
             thdinput <- forkIO (input chan)
-            mapM_ (forkIO . flip salutate chan) chans
-            thds <- mapM (forkIO . flip shameless chan) chans
+            mapM_ (forkIO . flip salutate chan) (map T.decodeUtf8 chans)
+            thds <- mapM (forkIO . flip shameless chan) (map T.decodeUtf8 chans)
             send cfg handle chan
             mapM_ killThread thds
             killThread thdrecv
             killThread thdinput
 
-salutate :: ByteString -> Chan Event -> IO ()
+salutate :: Text -> Chan Event -> IO ()
 salutate channel chan = do
     intro <- anyElem introductions
-    let evt = Send $ IRC.Message Nothing "PRIVMSG" [channel, intro]
+    let evt = Send $ IRC.Message Nothing "PRIVMSG" [T.encodeUtf8 channel, T.encodeUtf8 intro]
     writeChan chan evt
     
    
-introductions :: [ByteString] 
+introductions :: [Text] 
 introductions = ["hi","hello","hey","hola","sup"]
 
-shameless :: ByteString -> Chan Event -> IO ()
+shameless :: Text -> Chan Event -> IO ()
 shameless channel chan = do
     r <- Random.randomRIO (300 * 12, 300 * 24)
     threadDelay (r * 1000000)
     interruption <- anyElem interruptions
-    let evt = Send $ IRC.Message Nothing "PRIVMSG" [channel, interruption]
+    let evt = Send $ IRC.Message Nothing "PRIVMSG" [T.encodeUtf8 channel, T.encodeUtf8 interruption]
     -- writeChan chan evt
     shameless channel chan
    
 
-interruptions :: [ByteString]
+interruptions :: [Text]
 interruptions =
     ["hueueueueue","lol","curvature","zbrt","woop woop woop","zqck"] ++ introductions
 
@@ -97,16 +101,16 @@ input chan = do
     writeChan chan (RawMessage line)
     input chan
 
-login :: ByteString -> Maybe ByteString -> Chan Event -> IO ()
+login :: Text -> Maybe Text -> Chan Event -> IO ()
 login nck mpswd chan = do
-    writeChan chan (Send $ IRC.nick nck)
-    writeChan chan (Send $ IRC.user nck "0" "*" nck)
+    writeChan chan (Send $ IRC.nick (T.encodeUtf8 nck))
+    writeChan chan (Send $ IRC.user (T.encodeUtf8 nck) "0" "*" (T.encodeUtf8 nck))
     case mpswd of 
         Nothing -> return ()
         Just pswd -> writeChan chan (Send $ identify pswd)
 
-joinChannel :: [ByteString] -> Chan Event -> IO ()
-joinChannel chans chan = mapM_ (writeChan chan . Send . IRC.joinChan) chans
+joinChannel :: [Text] -> Chan Event -> IO ()
+joinChannel chans chan = mapM_ (writeChan chan . Send . IRC.joinChan) (map T.encodeUtf8 chans)
 
 recv :: Handle -> Chan Event -> IO ()
 recv conn chan = do
@@ -160,24 +164,24 @@ replyMsg cfg (IRC.Message (Just (IRC.NickName sender _ _)) command params) = do
                     $ IRC.Message Nothing "PRIVMSG" [asker, BS.empty, str]
     case Safe.readMay (BSC.unpack command) of
         Just PRIVMSG -> do
-            result <- Parsec.runPT (response nick sender) () "" body
-            either (const $ return Nothing) respond result
+            result <- Parsec.runPT (response (T.decodeUtf8 nick) (T.decodeUtf8 sender)) () "" (T.decodeUtf8 body)
+            either (const $ return Nothing) respond (fmap T.encodeUtf8 result)
         _ -> return Nothing
 replyMsg _ _ = return Nothing
 
-response :: ByteString -> ByteString -> Parser ByteString
+response :: Text -> Text -> Parser Text
 response self sender = foldr1 (<|>)
     [ try $ zsayP >> space >> say
     , do void $ bstrings introductions
          void $ many1 space
-         void $ string (BSC.unpack self)
+         void $ string (T.unpack self)
          liftIO (anyElem introductions)
     , addressResponse self sender
     ]
 
-addressResponse :: ByteString -> ByteString -> Parser ByteString
+addressResponse :: Text -> Text -> Parser Text
 addressResponse self sender = do
-    void (string (BSC.unpack self))
+    void (string (T.unpack self))
     void addressSuffix
     foldr1 (<|>)
         [ try (string "say") >> space >> say
@@ -187,15 +191,15 @@ addressResponse self sender = do
         , magic8Prefix >> many1 space >> liftIO magic8Response
         ]
 
-anagram :: (String -> Bool) -> Parser ByteString
+anagram :: (String -> Bool) -> Parser Text
 anagram isWord = do
-    word <- BSC.pack <$> manyTill Parsec.alphaNum (void space <|> eof)
-    if BS.length word >= 10
+    word <- T.pack <$> manyTill Parsec.alphaNum (void space <|> eof)
+    if T.length word >= 10
        then parserFail ""
-       else return . BS.intercalate " " . anagrams isWord $ word
+       else return . T.intercalate " " . anagrams isWord $ word
 
-say :: Parser ByteString
-say = BSC.pack . Say.zsay <$> manyTill anyChar eof
+say :: Parser Text
+say = T.pack . Say.zsay <$> manyTill anyChar eof
 
 addressSuffix :: Parser String
 addressSuffix = strings [" ",", ",": "]
@@ -209,42 +213,42 @@ anagramP = string "anagram"
 strings :: [String] -> Parser String
 strings = choice . map (try . string)
 
-bstrings :: [ByteString] -> Parser ByteString
-bstrings = fmap BSC.pack . strings . map BSC.unpack
+bstrings :: [Text] -> Parser Text
+bstrings = fmap T.pack . strings . map T.unpack
 
-hal9000Prefix :: Parser ByteString
+hal9000Prefix :: Parser Text
 hal9000Prefix = bstrings
     ["will you open", "can you open", "may you open","open the"]
 
-magic8Prefix :: Parser ByteString
+magic8Prefix :: Parser Text
 magic8Prefix = bstrings
     ["was","were ","will","do","did","does","is","are","can","have","has","would","could"]
 
-magic8Response :: IO ByteString
+magic8Response :: IO Text
 magic8Response = anyElem
     ["yes","no","idk. ask zrkw","sometimes","correct","probably","pository","negatory"
     ,"possibly. query the pcercuei. he would know.","maybe...", "huh?", "sure", "nope"
     ,"yeah", "yup","yes", "no"]
 
-hal9000Response :: ByteString -> ByteString
-hal9000Response n = BS.concat ["sorry, ", n,". I can't do that."]
+hal9000Response :: Text -> Text
+hal9000Response n = T.concat ["sorry, ", n,". I can't do that."]
 
 anyElem :: [b] -> IO b
 anyElem xs = do
     idx <- Random.randomRIO (0, length xs -1)
     return $ xs !! idx
 
-solveBestHand :: ByteString -> ByteString
+solveBestHand :: Text -> Text
 solveBestHand _ = "fold, human"
 
 prefixWith :: String -> String -> Bool
 prefixWith xs ys = and $ zipWith (==) xs ys
 
-identify :: ByteString -> IRC.Message
-identify pswd = IRC.Message Nothing "PRIVMSG" ["nickserv", ":identify", pswd]
+identify :: Text -> IRC.Message
+identify pswd = IRC.Message Nothing "PRIVMSG" ["nickserv", ":identify", T.encodeUtf8 pswd]
 
 zmsg :: String -> String -> IRC.Message
 zmsg target msg = IRC.Message Nothing "PRIVMSG" (map BSC.pack [target, ' ':(Say.zsay msg)])
 
-anagrams :: (String -> Bool) -> ByteString -> [ByteString]
-anagrams isWord = map BSC.pack . filter isWord . L.nub . L.permutations . BSC.unpack
+anagrams :: (String -> Bool) -> Text -> [Text]
+anagrams isWord = map T.pack . filter isWord . L.nub . L.permutations . T.unpack
--- src/Zb0t/Poker.hs
+++ src/Zb0t/Poker.hs
@@ -1,40 +1,36 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Zb0t.Poker where
 
-
-import           Data.Functor (void)
-import           Control.Applicative hiding ((<|>))
+import Control.Lens
+import HoldEm hiding (turn)
+import Control.Applicative hiding ((<|>))
 import qualified Data.Map as Map
 import qualified System.Random as Random
 import qualified Text.Parsec as Parsec
 import qualified Text.Parsec.String as Parsec
-import           Text.Parsec ((<|>))
-import           Control.Lens
-import qualified HoldEm as HoldEm
+import Text.Parsec ((<|>))
+import Data.Functor (void)
 
+r2, r3, r4, r5, r6, r7, r8, r9, r10, jack, queen, king, ace :: Parsec.Parser Rank
+r2 = Parsec.string "2" >> return R2
+r3 = Parsec.string "3" >> return R3
+r4 = Parsec.string "4" >> return R4
+r5 = Parsec.string "5" >> return R5
+r6 = Parsec.string "6" >> return R6
+r7 = Parsec.string "7" >> return R7
+r8 = Parsec.string "8" >> return R8
+r9 = Parsec.string "9" >> return R9
+r10 = Parsec.string "10" >> return R10
+jack = Parsec.string "J" >> return Jack
+queen = Parsec.string "Q" >> return Queen
+king = Parsec.string "K" >> return King
+ace = Parsec.string "A" >> return Ace
 
-r2, r3, r4, r5, r6, r7, r8, r9, r10, jack, queen, king, ace :: Parsec.Parser HoldEm.Rank
-r2 = Parsec.string "2" >> return HoldEm.R2
-r3 = Parsec.string "3" >> return HoldEm.R3
-r4 = Parsec.string "4" >> return HoldEm.R4
-r5 = Parsec.string "5" >> return HoldEm.R5
-r6 = Parsec.string "6" >> return HoldEm.R6
-r7 = Parsec.string "7" >> return HoldEm.R7
-r8 = Parsec.string "8" >> return HoldEm.R8
-r9 = Parsec.string "9" >> return HoldEm.R9
-r10 = Parsec.string "10" >> return HoldEm.R10
-jack = Parsec.string "J" >> return HoldEm.Jack
-queen = Parsec.string "Q" >> return HoldEm.Queen
-king = Parsec.string "K" >> return HoldEm.King
-ace = Parsec.string "A" >> return HoldEm.Ace
-
-
-clubs, hearts, diamonds, spades :: Parsec.Parser HoldEm.Suit
-clubs = Parsec.char 'c' >> return HoldEm.Clubs
-hearts = Parsec.char 'h' >> return HoldEm.Hearts
-diamonds = Parsec.char 'd' >> return HoldEm.Diamonds
-spades = Parsec.char 's' >> return HoldEm.Spades
-
+clubs, hearts, diamonds, spades :: Parsec.Parser Suit
+clubs = Parsec.char 'c' >> return Clubs
+hearts = Parsec.char 'h' >> return Hearts
+diamonds = Parsec.char 'd' >> return Diamonds
+spades = Parsec.char 's' >> return Spades
 
 check, fold, raise :: Parsec.Parser Move
 check = Parsec.string "check" >> return Check
@@ -45,32 +41,25 @@ raise = do
     val <- Parsec.many1 Parsec.digit
     return (Raise (read val))
 
-
-rank :: Parsec.Parser HoldEm.Rank
-rank = r2 <|> r3 <|> r4 <|> r5 <|> r6 <|> r7 <|> r8 <|> r9 <|> r10 <|>
+rank' :: Parsec.Parser Rank
+rank' = r2 <|> r3 <|> r4 <|> r5 <|> r6 <|> r7 <|> r8 <|> r9 <|> r10 <|>
        jack <|> queen <|> king <|> ace
 
+suit' :: Parsec.Parser Suit
+suit' = clubs <|> hearts <|> diamonds <|> spades
 
-suit :: Parsec.Parser HoldEm.Suit
-suit = clubs <|> hearts <|> diamonds <|> spades
+card :: Parsec.Parser Card
+card = Card <$> rank' <*> suit'
 
-
-card :: Parsec.Parser HoldEm.Card
-card = HoldEm.Card <$> rank <*> suit
-
-
-cards :: Parsec.Parser [HoldEm.Card]
+cards :: Parsec.Parser [Card]
 cards = Parsec.many $ do
     Parsec.many1 Parsec.space
     card
 
-
 move :: Parsec.Parser Move
 move = check <|> fold <|> raise
 
-
 type Name = String
-
 
 data Move
     = Check
@@ -78,30 +67,25 @@ data Move
     | Raise Integer
     deriving (Show, Eq)
 
-
 data Chip
     = Dealer
     | SmallBlind
     | BigBlind
     deriving (Show, Eq)
 
-
 data Player = Player
     { _points :: Integer
-    , _pHand :: HoldEm.PHand 
+    , _pHand :: PHand 
     } deriving (Show, Eq)
-
 
 data Pot = Pot
     { _contributors :: [Int]
     , _totalValue:: Integer
     } deriving (Show, Eq)
 
-
 data Config = Config
     { _playerNames :: [String]
     } deriving (Show, Eq)
-
 
 data Game = Game
     { _config :: Config
@@ -115,15 +99,13 @@ data Game = Game
     , _playerChips :: Map.Map Int Chip
     } deriving (Show, Eq)
 
-
 makeLenses ''Player
 makeLenses ''Pot
 makeLenses ''Config
 makeLenses ''Game
 
-
 doMove :: Int -> Game -> Move -> Game
 doMove n g m = case m of
-    Check -> g & over turn (+1)
-    FoldMove -> g & over turn (+1)
-    Raise value -> g & over turn (+1)
+    Check -> g & turn +~ 1
+    FoldMove -> g & turn +~ 1
+    Raise value -> g & turn +~ 1
